e17156736621fca792534a1daa7faf892b613aa1
XrioBtw <33559295+XrioBtw@users.noreply.github.com>
2020-07-16 02:01:32 +0200

    Add EffectiveLevelPluginTest.java

---
+++ src/test/java/com/effectivelevel/EffectiveLevelPluginTest.java
@@ -0,0 +1,13 @@
+package com.effectivelevel;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class EffectiveLevelPluginTest
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(EffectiveLevelPlugin.class);
+		RuneLite.main(args);
+	}
+}
